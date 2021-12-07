97fbecc6bb367028685b8025c27a898499f4f5b2
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-12-07 15:02:49 -0800

    cart items for specific user displayin and can tell total price

--- client/src/Cart.js
+++ client/src/Cart.js
@@ -1,6 +1,37 @@
+import { useState, useEffect } from 'react'
+import CartItem from './CartItem'
+
 function Cart(){
+    const [carts, setCarts] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch("/cartitems")
+          .then((res) => res.json())
+          .then((data) => {setCarts(data)
+            setIsLoading(false)
+        });
+    }, [])
+
+    if (isLoading) return <div>Page is loading</div>
+
+    const name = carts[0].user.name
+
+    const cart_item = carts.map(item => <CartItem key = {item.id} item = {item}/>)
+
+    let total = 0
+    const totalPrice = carts.map(item => {
+        total += item.product.price
+    })
+    
+
     return (
-        <div></div>
+        <div>
+            <h3>Hello {name}, here are the items in your cart:</h3>
+            {cart_item}
+            <h3>Total price: ${total}</h3>
+            <button>Checkout</button>
+        </div>
     )
 }
 export default Cart;
